aeefeea6c8e3f17b084b60a95e8a9aee12820a25
MattTurnock <matthew.turnock1@gmail.com>
2018-11-21 20:04:07 +0100

    fixed the funky conversion

--- Ass1/main.py
+++ Ass1/main.py
@@ -2,7 +2,8 @@ from Ass1 import Cart2Kep_utils, Kep2Cart_utils, kep_orbit_utils, misc_utils, la
 import numpy as np
 
 state_1_cartesian = [10157768.1264, -6475997.0091, 2421205.9518, 1099.2953996, 3455.1059240, 4355.0978095]        #in m and m/s
-state_2_keplerian = [12269687.5912, 0.004932091570, 109.823277603, 134.625563565, 106.380426142, 301.149932402]   #in m and degrees, with M given
+state_2_keplerian_degrees = [12269687.5912, 0.004932091570, 109.823277603, 134.625563565, 106.380426142, 301.149932402]   #in m and degrees, with M given
+state_2_keplerian = kep_orbit_utils.Kep_Deg2Rad(state_2_keplerian_degrees)
 
 state_1_keplerian = Cart2Kep_utils.Cart2Kep(state_1_cartesian)
 state_2_cartesian = Kep2Cart_utils.Kep2Cart(state_2_keplerian, given_angle='M')
@@ -24,3 +25,4 @@ print('\nState 1 [Cartesian, Keplerian]:\n',
 print('State 2 [Keplerian, cartesian]: \n',
       latex_utils.Kep2Latex(state_2_keplerian),
       latex_utils.Cart2Latex(state_2_cartesian_rounded))
+
