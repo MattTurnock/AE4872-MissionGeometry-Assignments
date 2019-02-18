f72f34874a7e47f4345a63b62e932ccd07e34430
MattTurnock <matthew.turnock@protonmail.com>
2019-02-18 22:04:33 +0100

    Ass6 - Part3 started

--- Ass6/part3.py
+++ Ass6/part3.py
@@ -9,5 +9,21 @@ __version__ = "1.0"
 
 ########################################################################################################################
 
+import numpy as np
 
+iterations = 3
+arrayLength = 26
+
+monteArrayBase = np.zeros((iterations, arrayLength))
+randIndices = [1, 4, 7, 9, 11, 13, 15, 17, 19, 21]
+
+########################################################################################################################
+# Do the random only case:
+
+monteRandArray = np.copy(monteArrayBase)
+
+# Plonk random numbers into the random slots
+monteRandArray[:, randIndices] = np.random.rand(iterations, len(randIndices))
+# monteRandArray[[0,1], :] = np.ones((2, 26))
+print(monteRandArray) #TODO: implement the multiplication of created randoms with the representative value (use an array to do it?)
 
