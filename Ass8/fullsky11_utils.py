ea942c35839e4fa3fa22511487750efa9e96ecda
MattTurnock <matthew.turnock1@gmail.com>
2019-03-09 15:13:29 +0000

    Ass8 - Initial Commit

---
+++ Ass8/fullsky11_utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+fullsky11_utils.py: A bunch of useful functions for the FULLSKY-11 assignment
+"""
+__author__      = "Matthew Turnock"
+__email__ = "matthew.turnock@protonmail.com"
+__version__ = "1.0"
+
+########################################################################################################################
+
+import numpy as np
