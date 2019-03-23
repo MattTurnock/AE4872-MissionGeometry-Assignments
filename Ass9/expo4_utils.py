9d9a2f243d7428c62eb6b1dfe53f05b94e31ba81
MattTurnock <matthew.turnock@protonmail.com>
2019-03-23 22:08:11 +0100

    Ass9 - Finished utils and verification

--- Ass9/expo4_utils.py
+++ Ass9/expo4_utils.py
@@ -18,15 +18,20 @@ pi = np.pi
 
 ######################################################################################################################
 
-muSun = constants["muSun"]
+# muSun = constants["muSun"]
+#
+# r1 = (1 * u.AU)
+# r2 = 1.5 * u.AU
+# k2 = 1/12
+# Psi = 90*u.deg
+# N = 2
+# thetaExample = 0.0*u.rad
+# gamma1Example = -80*u.deg
 
-r1 = 1 * u.AU
-r2 = 1.5 * u.AU
-k2 = 1/12
-Psi = 90*u.deg
-N = 2
-thetaExample = 0.0*u.rad
-gamma1Example = -80*u.deg
+
+
+
+
 
 def getgamma1Min(r1, r2, k2, thetaBar, Delta, outputUnits=None):
     """
@@ -50,6 +55,10 @@ def getgamma1Min(r1, r2, k2, thetaBar, Delta, outputUnits=None):
         gamma1Min = gamma1Min.to(outputUnits)
 
     return gamma1Min
+
+
+
+
 
 
 def getgamma1Max(r1, r2, k2, thetaBar, Delta, outputUnits=None):
@@ -77,6 +86,10 @@ def getgamma1Max(r1, r2, k2, thetaBar, Delta, outputUnits=None):
 
 
 
+
+
+
+
 def getDelta(r1, r2, k2, thetaBar):
     """
     Get intermediate parameter Delta for previous functions. Dimensionless therefore can use astropy units or not.
@@ -95,6 +108,12 @@ def getDelta(r1, r2, k2, thetaBar):
     Delta = term1 - term2
 
     return Delta
+
+
+
+
+
+
 
 
 
@@ -120,17 +139,29 @@ def getthetaBar(Psi, N, outputUnits=None):
 
 
 
-thetaBar = getthetaBar(Psi, N, outputUnits=u.rad)
-print("thetaBar : %s" %thetaBar)
 
-Delta = getDelta(r1, r2, k2, thetaBar)
-print("Delta : %s" %Delta)
 
-gamma1Min = getgamma1Min(r1, r2, k2, thetaBar, Delta, outputUnits=u.deg)
-print("gamma1Min : %s" %gamma1Min)
 
-gamma1Max = getgamma1Max(r1, r2, k2, thetaBar, Delta, outputUnits=u.deg)
-print("gamma1Max : %s" %gamma1Max)
+
+
+
+# thetaBar = getthetaBar(Psi, N, outputUnits=u.rad)
+# print("thetaBar : %s" %thetaBar)
+#
+# Delta = getDelta(r1, r2, k2, thetaBar)
+# print("Delta : %s" %Delta)
+#
+# gamma1Min = getgamma1Min(r1, r2, k2, thetaBar, Delta, outputUnits=u.deg)
+# print("gamma1Min : %s" %gamma1Min)
+#
+# gamma1Max = getgamma1Max(r1, r2, k2, thetaBar, Delta, outputUnits=u.deg)
+# print("gamma1Max : %s" %gamma1Max)
+
+
+
+
+
+
 
 
 
@@ -146,7 +177,7 @@ def getk1(r1, r2, k2, thetaBar, gamma1):
     :param gamma1: Chosen value for gamma1
     :return k1:
     """
-    print(r1, r2, k2)
+    # print(r1, r2, k2)
     top = np.log(r1/r2) + np.tan(gamma1)/k2 * sin(k2*thetaBar)
     bottom = 1 - cos(k2*thetaBar)
 
@@ -158,8 +189,23 @@ def getk1(r1, r2, k2, thetaBar, gamma1):
 
     return k1
 
-k1 = getk1(r1, r2, k2, thetaBar, gamma1Example)
-print("k1 : %s" %k1)
+
+
+
+
+
+
+
+#
+# k1 = getk1(r1, r2, k2, thetaBar, gamma1Example)
+# print("k1 : %s" %k1)
+#
+#
+
+
+
+
+
 
 
 
@@ -181,8 +227,23 @@ def getphi(k1, k2, gamma1, outputUnits=None):
 
 
 
-phi = getphi(k1, k2, gamma1Example, outputUnits=u.rad)
-print("phi : %s" %phi)
+
+
+
+
+
+
+
+# phi = getphi(k1, k2, gamma1Example, outputUnits=u.rad)
+# print("phi : %s" %phi)
+#
+
+
+
+
+
+
+
 
 
 def getk0(r1, k1, phi, outputUnits=None):
@@ -201,8 +262,25 @@ def getk0(r1, k1, phi, outputUnits=None):
 
     return k0
 
-k0 = getk0(r1, k1, phi, outputUnits=u.km)
-print("k0 : %s" %k0)
+
+
+
+
+
+
+
+
+#
+# k0 = getk0(r1, k1, phi, outputUnits=u.km)
+# print("k0 : %s" %k0)
+
+
+
+
+
+
+
+
 
 
 def getS(k2, theta, phi):
@@ -217,8 +295,24 @@ def getS(k2, theta, phi):
 
     return s
 
-s = getS(k2, thetaExample, phi)
-print("s : %s" %s)
+
+
+
+
+
+
+
+#
+# s = getS(k2, thetaExample, phi)
+# print("s : %s" %s)
+
+
+
+
+
+
+
+
 
 
 def getC(k2, theta, phi):
@@ -233,26 +327,69 @@ def getC(k2, theta, phi):
 
     return c
 
-c = getC(k2, thetaExample, phi)
-print("c : %s" %c)
+
+
+
+
+
+
+#
+# c = getC(k2, thetaExample, phi)
+# print("c : %s" %c)
+
+
+
+
+
+
+
+
 
 
 def gettangamma(k1, k2, theta, phi):
+    """
+    Finds tan(gamma) using equation from slides
+    :param k1:
+    :param k2:
+    :param theta:
+    :param phi:
+    :return tangamma:
+    """
 
     tangamma = k1 * k2 * cos(k2*theta + phi)
 
     return tangamma
 
-tangamma = gettangamma(k1, k2, thetaExample, phi)
-print("tan gamma : %s" %tangamma)
 
-gamma = np.arctan(tangamma)
-print("gamma : %s" %gamma)
+
+
+
+
+
+
+# tangamma = gettangamma(k1, k2, thetaExample, phi)
+# print("tan gamma : %s" %tangamma)
+#
+# gamma = np.arctan(tangamma)
+# print("gamma : %s" %gamma)
+
+
+
+
+
+
+
 
 
 
 def getaccelLocal(r, mu, outputUnits=None):
-
+    """
+    Finds local acceleration at a given point from central body. If no units given use SI
+    :param r:
+    :param mu:
+    :param outputUnits:
+    :return:
+    """
     aLocal = mu/r**2
 
     if outputUnits is not None:
@@ -260,12 +397,41 @@ def getaccelLocal(r, mu, outputUnits=None):
 
     return aLocal
 
-aLocal = getaccelLocal(r1, muSun, outputUnits=u.m/u.s**2)
-print("Local accel : %s" %aLocal)
 
-def getaccel(k1, k2, gamma, s, r, mu, outputUnits=None):
 
-    out = np.tan(gamma) / 2*cos(gamma)
+
+
+
+
+
+
+# aLocal = getaccelLocal(r1, muSun, outputUnits=u.m/u.s**2)
+# print("Local accel : %s" %aLocal)
+
+
+
+
+
+
+
+
+
+
+def getaccel(k1, k2, gamma, s, r, mu, outputUnits=None, alwaysPositive=False):
+    """
+    Gets the thrusting acceleration using equations given in slides. If no units given use SI
+    :param k1:
+    :param k2:
+    :param gamma:
+    :param s:
+    :param r:
+    :param mu:
+    :param outputUnits:
+    :param alwaysPositive: Determines whether to do as in Petropoulos (with a value for n to make accel always positive)
+    or to leave as usual, and a negative acceleration indicates against the spacecraft velocity
+    :return accel, n: When alwaysPositive is False, n is uneccessary and is equal to 0
+    """
+    out = np.tan(gamma) / (2*cos(gamma))
     in1 = ( (np.tan(gamma))**2 + k1*k2**2*s + 1 )**-1
 
     in2_top = k2**2 * (1 - 2*k1*s)
@@ -274,26 +440,60 @@ def getaccel(k1, k2, gamma, s, r, mu, outputUnits=None):
     #accelN is normalised acceleration wrt solar accel
     accelNTemp = out * (in1 - in2_top/in2_bottom)
 
-    if np.sign(accelNTemp) == 1:
-        n = 0
+    if alwaysPositive is True:
+        if np.sign(accelNTemp) == 1:
+            n = 0
+        else:
+            n=1
     else:
-        n=1
+        n=0
 
     accelN = (-1)**n * accelNTemp
 
     accel = accelN * getaccelLocal(r, mu)
+
 
     if outputUnits is not None:
         accel = accel.to(outputUnits)
 
     return accel, n
 
-accel, n = getaccel(k1, k2, gamma, s, r1, muSun, outputUnits=u.km/u.s**2)
-print("Acceleration : %s In %s direction" %(accel, n))
+
+
+
+
+
+
+
+
+
+
+# accel, n = getaccel(k1, k2, gamma, s, r1, muSun, outputUnits=u.km/u.s**2, alwaysPositive=False)
+# print("Acceleration : %s In %s direction" %(accel, n))
+
+
+
+
+
+
+
+
+
+
 
 
 def getdtdtheta(k1, k2, gamma, r, s, mu, outputUnits=None):
-
+    """
+    Gets inverse rate of change of theta. If no units given use SI
+    :param k1:
+    :param k2:
+    :param gamma:
+    :param r:
+    :param s:
+    :param mu:
+    :param outputUnits:
+    :return dtdtheta:
+    """
     top = r**3 * ( np.tan(gamma)**2 + k1*k2**2*s + 1)
 
     dtdtheta = np.sqrt(top/mu)
@@ -303,12 +503,38 @@ def getdtdtheta(k1, k2, gamma, r, s, mu, outputUnits=None):
 
     return dtdtheta
 
-dtdtheta2 = getdtdtheta(k1, k2, gamma, r1, s, muSun, outputUnits=u.s/u.rad)**2
-print("dt/dtheta^2 : %s" %dtdtheta2)
+
+
+
+
+
+
+
+
+# dtdtheta = getdtdtheta(k1, k2, gamma, r1, s, muSun, outputUnits=u.s/u.rad)
+# dtdtheta2 = dtdtheta**2
+# print("dt/dtheta^2 : %s" %dtdtheta2)
+
+
+
+
+
+
+
+
 
 
 def getr(k0, k1, k2, theta, phi, outputUnits=None):
-
+    """
+    Gets radius from central body with equations as shown in slides. If no units given use SI
+    :param k0:
+    :param k1:
+    :param k2:
+    :param theta:
+    :param phi:
+    :param outputUnits:
+    :return:
+    """
     r = k0 * np.exp(k1 * np.sin(k2*theta + phi))
 
     if outputUnits is not None:
@@ -316,8 +542,210 @@ def getr(k0, k1, k2, theta, phi, outputUnits=None):
 
     return r
 
-r = getr(k0, k1, k2, thetaExample, phi, outputUnits=u.km)
-print("r : %s" %r)
+
+
+
+
+
+
+
+
+
+
+# r = getr(k0, k1, k2, thetaExample, phi, outputUnits=u.km)
+# print("r : %s" %r)
+
+
+
+
+
+def getdt(dtheta, dtdtheta, outputUnits=None):
+    #TODO: might ditch this function (see TOF)
+    """
+    Gets value for t given dtdtheta. If no units given use SI
+    :param dtheta: find from table with theta_(i+1) - theta_(i)
+    :param dtdtheta: from previous equations
+    :param outputUnits:
+    :return dt:
+    """
+    dt = dtdtheta * dtheta
+
+    if outputUnits is not None:
+        dt.to(outputUnits)
+
+    return dt
+
+
+
+
+
+# theta_2 = 1.4137167*u.rad
+# dtheta = theta_2 - thetaExample
+# dt = getdt(dtheta, dtdtheta, outputUnits=u.s)
+# print("dt : %s" %dt)
+#
+# tf = 4.045019805E7 * u.s
+# t0 = 4.044862610E7 * u.s
+# print((tf).to(u.year))
+
+
+
+
+
+
+
+
+
+def doTOFFunc(k1, k2, gamma, r, s, mu, dtheta, outputUnits=None):
+    """
+    Performs function inside the sum for TOF calcs (can be used for rectangle or simpson) If no units given use SI
+    :param k1:
+    :param k2:
+    :param gamma:
+    :param r:
+    :param s:
+    :param mu:
+    :param dtheta:
+    :param outputUnits:
+    :return fx:
+    """
+    top = r**3 * (np.tan(gamma)**2 + k1*k2**2*s + 1)
+
+    fx = dtheta * np.sqrt(top/mu)
+
+    if outputUnits is not None:
+        fx = fx.to(outputUnits, equivalencies=u.dimensionless_angles())
+
+    return fx
+
+
+
+# fx0 = doTOFFunc(k1, k2, gamma, r, s, muSun, dtheta, outputUnits=u.s)
+# print("fx0 : %s" %fx0)
+
+
+
+def doExposinStep(theta, dtheta, k0, k1, k2, phi, mu, rUnits=None, accelUnits=None, dtdthetaUnits=None, timeUnits=None):
+
+    s = getS(k2, theta, phi)
+    k1s = k1*s
+    c = getC(k2, theta, phi)
+    tangamma = gettangamma(k1, k2, theta, phi)
+    gamma = np.arctan(tangamma)
+    r = getr(k0, k1, k2, theta, phi, outputUnits=rUnits)
+    accel, n = getaccel(k1, k2, gamma, s, r, mu, outputUnits=accelUnits, alwaysPositive=False)
+    dtdtheta = getdtdtheta(k1, k2, gamma, r, s, mu, outputUnits=dtdthetaUnits)
+    dtdtheta2 = dtdtheta**2
+
+    fx = doTOFFunc(k1, k2, gamma, r, s, mu, dtheta, outputUnits=timeUnits)
+
+    return [k1s, s, c, tangamma, gamma, r, accel, dtdtheta2, fx]
+
+# dtheta = 1.4137167 * u.rad
+#
+# theta0 = 0 * dtheta
+# point0List = doExposinStep(theta0, dtheta, k0, k1, k2, phi, muSun, rUnits=u.km, accelUnits=u.km/u.s**2, dtdthetaUnits=u.s/u.rad, timeUnits=u.s)
+# fx0 = point0List[-1]
+# print("Point 0 things : %s" %point0List)
+
+
+
+
+
+
+
+
+
+
+
+def doFullExposin(k2, r1, r2, Psi, N, gamma1, mu, method="rectangle_central", thetaSplitNumber=10,
+                  angleUnits=None, distanceUnits=None, accelUnits=None, timeUnits=None):
+    """
+    Do the exposin simulation for a specific set of parameters. To run for many N's etc, use this function repeatedly
+    :param k2:
+    :param r1:
+    :param r2:
+    :param Psi:
+    :param N:
+    :param gamma1:
+    :param mu:
+    :param method: Whether to use central rectangle method or start of section method. Simpson not yet implemented
+    :param thetaSplitNumber: Number of points to use when splitting thetaBar
+    :param angleUnits:
+    :param distanceUnits:
+    :param accelUnits:
+    :param timeUnits:
+    :return:
+    """
+    # Find thetaBar and split the theta search space using central rectangle theorem
+    thetaBar = getthetaBar(Psi, N, outputUnits=angleUnits)
+    thetaArrBase = np.linspace(0, thetaBar, thetaSplitNumber + 1)[0:-1]
+    dtheta = thetaArrBase[1] - thetaArrBase[0]
+    if method == "rectangle_lower":
+        thetaArr = thetaArrBase
+
+    elif method == "rectangle_central":
+        thetaArr = thetaArrBase + dtheta/2
+
+    # Big array to put all data in (like the one on slides)
+    if angleUnits is not None:
+        dataFinal = np.zeros((len(thetaArr), 11), dtype=object)
+    else:
+        dataFinal = np.zeros((len(thetaArr), 11), dtype=float)
+
+    dataFinal[:, 0] = thetaArr
+    # print(dataFinal)
+
+    # Calculate/ set common parameters
+    k1 = getk1(r1, r2, k2, thetaBar, gamma1)
+    phi = getphi(k1, k2, gamma1, outputUnits=angleUnits)
+    k0 = getk0(r1, k1, phi, outputUnits=distanceUnits)
+
+    fxList = []
+    # print(thetaArr)
+    for i in range(len(thetaArr)):
+        theta = thetaArr[i]
+
+        if (timeUnits is None) or (angleUnits is None):
+            dtdthetaUnits = None
+        else:
+            dtdthetaUnits = timeUnits/angleUnits
+
+        k1s, s, c, tangamma, gamma, r, accel, dtdtheta2, fx = doExposinStep(theta, dtheta, k0, k1, k2, phi, mu,
+                      rUnits=distanceUnits, accelUnits=accelUnits, dtdthetaUnits=dtdthetaUnits, timeUnits=timeUnits)
+
+        # Make time the sum
+        fxList.append(fx)
+        t = sum(fxList)
+        dt = fx
+        # print(k1s, s, c, tangamma, gamma, r, accel, dtdtheta2, dt, t)
+        dataFinal[i, 1:] = k1s, s, c, tangamma, gamma, r, accel, dtdtheta2, dt, t
+
+    return dataFinal
+
+# print(doFullExposin(k2, r1, r2, Psi, N, gamma1Example, muSun, method="rectangle_central", thetaSplitNumber=10,
+#                   angleUnits=u.rad, distanceUnits=u.km, accelUnits=u.km/u.s**2, timeUnits=u.s))
+
+
+
+
+
+
+
+
+
+# theta1 = 1 * dtheta
+# point1List = doExposinStep(theta1, dtheta, k0, k1, k2, phi, muSun, rUnits=u.km, accelUnits=u.km/u.s**2, dtdthetaUnits=u.s/u.rad, timeUnits=u.s)
+# fx1 = point1List[-1]
+# print("Point 1 things : %s" %point1List)
+#
+# theta2 = 2 * dtheta
+# point2List = doExposinStep(theta2, dtheta, k0, k1, k2, phi, muSun, rUnits=u.km, accelUnits=u.km/u.s**2, dtdthetaUnits=u.s/u.rad, timeUnits=u.s)
+# fx2 = point2List[-1]
+# print("Point 2 things : %s" %point2List)
+#
+# TOF = (1/3 * dtheta * (fx0 + 4*fx1 + fx2)).to(u.s, equivalencies=u.dimensionless_angles())
+# print("Simpson TOF : %s" %TOF)
 
 
 
---
+++ Ass9/expo4_verification.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+"""
+expo4_verification.py: Verification script for the EXPO-4 assignment
+"""
+__author__      = "Matthew Turnock"
+__email__ = "matthew.turnock@protonmail.com"
+__version__ = "1.0"
+
+########################################################################################################################
+import numpy as np
+from matplotlib import pyplot as plt
+
+from Ass6.design7_utils import npArray2LatexTable
+from Ass9.expo4_utils import *
+from json_to_dict import constants
+
+calculating = False
+showing = True
+printing = True
+
+##########################################################################################################
+# Do example case, with N=2 etc
+
+muSun = constants["muSun"].decompose().value
+
+r1 = (1 * u.AU).decompose().value
+r2 = (1.5 * u.AU).decompose().value
+k2 = 1/12
+Psi = (90*u.deg).decompose().value
+N = 2
+
+gamma1Example = (-80*u.deg).decompose().value
+
+dataExample = doFullExposin(k2, r1, r2, Psi, N, gamma1Example, muSun)
+
+dataToSave1 = np.zeros( (len(dataExample), 6) )
+dataToSave1[:,0] = dataExample[:,0]
+dataToSave2 = np.copy(dataToSave1)
+
+dataToSave1[:, 1:] = dataExample[:, 1:6]
+dataToSave2[:, 1:] = dataExample[:, 6:]
+
+formatList1 = ["%.7f", "%.3f", "%.3f", "%.3f", "%.8f", "%.7f"]
+formatList2 = ["%.7f", "%.6E", "%.6E", "%.9E", "%.8E", "%.9E"]
+formatListMaster = ["%.7f", "%.3f", "%.3f", "%.3f", "%.8f", "%.7f", "%.6E", "%.6E", "%.9E", "%.8E", "%.9E"]
+
+npArray2LatexTable(dataExample, "verificationData/dataAll.txt", fmt=formatListMaster)
+npArray2LatexTable(dataToSave1, "verificationData/dataPart1.txt", fmt=formatList1)
+npArray2LatexTable(dataToSave2, "verificationData/dataPart2.txt", fmt=formatList2)
+
+########## Get TOF for example case with different numbers of evals ################
+
+stepNumbers = [10, 100, 1000, 2000, 10000]
+TOFs = np.zeros(len(stepNumbers))
+
+if calculating:
+
+    for i in range(len(stepNumbers)):
+        stepNumber = stepNumbers[i]
+
+        dataStep = doFullExposin(k2, r1, r2, Psi, N, gamma1Example, muSun, thetaSplitNumber=stepNumber)
+        TOF = (dataStep[-1, -1] * u.s).to(u.year).value
+        TOFs[i] = TOF
+
+    np.save("verificationData/TOFData.npy", TOFs)
+
+else:
+    TOFs = np.load("verificationData/TOFData.npy")
+
+for i in range(len(TOFs)):
+    TOF = TOFs[i]
+    stepNumber = stepNumbers[i]
+    print("Steps %s  : %s" % (stepNumber, TOF))
+########## Do TOF plots for many gammas ################
+# Get gamma1min and gamma1max
+thetaBar = getthetaBar(Psi, N)
+Delta = getDelta(r1, r2, k2, thetaBar)
+gamma1Min = getgamma1Min(r1, r2, k2, thetaBar, Delta, outputUnits=None)
+gamma1Max = getgamma1Max(r1, r2, k2, thetaBar, Delta)
+gamma1Max = np.deg2rad(5)
+
+gamma1s = np.linspace(gamma1Min, gamma1Max, 100)
+
+TOFGammaData = np.zeros( (len(gamma1s), len(stepNumbers)) )
+
+# stepNumbers = [10, 100]
+
+if calculating:
+
+    for i in range(len(gamma1s)):
+        for j in range(len(stepNumbers)):
+            gamma1 = gamma1s[i]
+            stepNumber = stepNumbers[j]
+            if printing: print("gamma %s, step %s" % (gamma1, stepNumber))
+
+            dataStep = doFullExposin(k2, r1, r2, Psi, N, gamma1, muSun, thetaSplitNumber=stepNumber)
+            TOF = (dataStep[-1, -1] * u.s).to(u.year).value
+            TOFGammaData[i, j] = TOF
+
+    np.save("verificationData/TOFGammaData.npy", TOFGammaData)
+
+else:
+    TOFGammaData = np.load("verificationData/TOFGammaData.npy")
+
+
+# print(TOFGammaData)
+
+gamma1s_degrees = np.rad2deg(gamma1s)
+
+labels = ["10 steps", "100 steps", "1000 steps", "2000 steps", "10000 steps"]
+xlabel = r"$\gamma_1$ [deg]"
+ylabel = "TOF [yrs]"
+savenameBase = "verificationData/verificationPlot_%s.pdf"
+
+plt.figure()
+for i in range(len(stepNumbers)):
+    plt.plot(gamma1s_degrees, TOFGammaData[:, i])
+plt.xlim([-90, 90])
+plt.ylim([0, 5])
+plt.legend(labels)
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.grid()
+plt.savefig(savenameBase %"main")
+
+plt.figure()
+for i in range(len(stepNumbers)):
+    plt.plot(gamma1s_degrees, TOFGammaData[:, i])
+
+plt.xlim([-90, -30])
+plt.ylim([0, 0.5])
+plt.legend(labels)
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.grid()
+plt.savefig(savenameBase %"cropped")
+if showing: plt.show()
+
+
+
